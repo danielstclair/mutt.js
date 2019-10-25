ecd9c24a79c7b31508bb06efb53b9309e6e7ae96
Daniel St. Clair <danielallanstclair@gmail.com>
2019-10-25 18:27:26 -0500

    Refactor isType method to match docs

--- src/methods/isType.js
+++ src/methods/isType.js
@@ -1,39 +1,64 @@
-const { matchesSchema } = require('../helpers/matchesSchema');
 const { getType } = require('../helpers/getType');
 const { checkType } = require('../helpers/checkType');
-// const {
-//   yieldError,
-//   typeErrorMessage,
-// } = require('../helpers/typeErrorMessages');
+const { getSharedKeys } = require('../helpers/getSharedKeys');
 
-const isType = (...types) => (target, options) => {
+const matchesObjectSchema = (currentType, target, cb) => {
+  const sharedKeys = getSharedKeys(currentType, target);
+  if (!sharedKeys.length) return false;
+
+  return sharedKeys.reduce((prev, curr) => {
+    const targetA = currentType[curr];
+    const targetB = target[curr];
+    const nestedType = getType(targetA);
+    if (nestedType === 'Function') {
+      const assessType = targetA(targetB);
+      if (!prev || !assessType) return false;
+      return true;
+    }
+    return cb(targetA)(target[curr]);
+  }, true);
+};
+
+const matchesArraySchema = (currentType, target) =>
+  target.reduce((prev, curr) => {
+    const [fn] = currentType;
+    const targetType = fn(curr);
+    if (!prev || !targetType) return false;
+    return true;
+  }, true);
+
+const isType = (...types) => (target) => {
   if (!types || !checkType(types, 'array')) {
     throw new Error(
       'At least one or more types should be passed in the "isType" function',
     );
   }
-  // const errorMethod = options ? options.errorMethod :
   let matchesType;
-  while (types.length > 0) {
-    const currentType = types.pop();
+  const currentTypes = [].concat(types);
+  while (currentTypes.length > 0) {
+    const currentType = currentTypes.pop();
     const metaType = getType(currentType);
+
     switch (metaType) {
       case 'String': {
         const curr = currentType.length ? currentType : metaType;
         matchesType = checkType(target, curr);
         break;
       }
+
       case 'Object': {
-        const prefix = options ? options.prefix : '_type';
-        // TODO this is messy
-        matchesType = matchesSchema(currentType, prefix)(target).success;
+        matchesType = matchesObjectSchema(currentType, target, isType);
         break;
       }
+
       case 'Array': {
-        // TODO
-        matchesType = true;
+        if (!checkType(target, 'array')) {
+          throw new Error('Expected target to be an array');
+        }
+        matchesType = matchesArraySchema(currentType, target);
         break;
       }
+
       default: {
         matchesType = checkType(target, metaType);
         break;
--- tests/methods/isType.spec.js
+++ tests/methods/isType.spec.js
@@ -10,11 +10,57 @@ describe('isType', () => {
     expect(isType('undefined')(undefined)).toBeTruthy();
     expect(isType('number', 'string', 'array')([])).toBeTruthy();
   });
+
   it('determines if an object matches a schema', () => {
-    const schema1 = { name: { _type: 'string' } };
+    const schema1 = { name: isType('string') };
     const user1 = { lalala: 'lalala' };
     const user2 = { name: 'jim' };
+    const test = isType(schema1)(user2);
     expect(isType(schema1)(user1)).toBeFalsy();
-    expect(isType(schema1)(user2)).toBeTruthy();
+    expect(test).toBeTruthy();
+  });
+
+  it('handles a deeply nested object', () => {
+    const schema = {
+      name: isType('string'),
+      meta: { email: isType('string') },
+    };
+    const user = { name: '1', meta: { email: 'test@test.com' } };
+    expect(isType(schema)(user)).toBeTruthy();
+  });
+
+  it('handles an array', () => {
+    const schema = [isType('string')];
+    const target1 = ['Jack', 'Diane'];
+    const target2 = ['Jack', 1];
+    const test1 = isType(schema)(target1); // true
+    const test2 = isType(schema)(target2); // true
+    expect(test1).toBeTruthy();
+    expect(test2).toBeFalsy();
+  });
+  it('handles array of schemas', () => {
+    const userSchema = {
+      user: {
+        firstName: isType('string', 'null'),
+        lastName: isType('string', 'null'),
+      },
+      auth: {
+        token: isType(''), // shorthand
+        tokenExpirationDate: isType('date'),
+      },
+    };
+
+    const user1 = {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+      auth: {
+        token: 'token',
+        tokenExpirationDate: new Date(),
+      },
+    };
+    const test = isType([isType(userSchema)])([user1]);
+    expect(test).toBeTruthy();
   });
 });
